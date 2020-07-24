1f0dfb9ed4c6e1feaeec68dacf395b29d9b7d98f
layouwen <layouwen@gmail.com>
2020-07-24 23:25:40 +0800

    使用 NavLink 高亮

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import React from "react";
 import Icon from "./Icon";
 
@@ -20,6 +20,12 @@ const NavWrapper = styled.nav`
            width: 24px;
            height: 24px;
          }
+         &.selected {
+            color: red;
+            > .icon {
+              fill: red;
+            }
+         }
       } 
     }
   }
@@ -30,22 +36,22 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="/tags">
+                    <NavLink to="/tags" activeClassName="selected">
                         <Icon name='tag'/>
                         标签
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/money">
+                    <NavLink to="/money" activeClassName="selected">
                         <Icon name='money'/>
                         记账
-                    </Link>
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/statistics">
+                    <NavLink to="/statistics" activeClassName="selected">
                         <Icon name='chart'/>
                         统计
-                    </Link>
+                    </NavLink>
                 </li>
             </ul>
         </NavWrapper>
