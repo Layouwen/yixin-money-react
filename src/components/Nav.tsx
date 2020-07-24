0392c90e957da888fb2f8fc830aeca22e4dca935
layouwen <layouwen@gmail.com>
2020-07-24 21:36:10 +0800

    安装 @types/webpack-env 以及自动引入svg图片

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
-require('icons/chart.svg')
-require('icons/money.svg')
-require('icons/tag.svg')
+import Icon from "./Icon";
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -30,21 +28,15 @@ const Nav = ()=>{
         <NavWrapper>
             <ul>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#tag"/>
-                    </svg>
+                    <Icon name='tag'/>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#money"/>
-                    </svg>
+                    <Icon name='money'/>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#chart"/>
-                    </svg>
+                    <Icon name='chart'/>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
