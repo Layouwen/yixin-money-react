a13198ad18ec15cc6bf2810ec7639ed98e708de4
layouwen <layouwen@gmail.com>
2020-07-24 23:10:33 +0800

    修复Icon点击范围

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,35 +9,43 @@ const NavWrapper = styled.nav`
   > ul {
     display: flex;
     > li {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 4px 0;
       width: 33.333%;
-      > .icon {
-        width: 24px;
-        height: 24px;
-      }
+      > a {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 4px 0;
+         > .icon {
+           width: 24px;
+           height: 24px;
+         }
+      } 
     }
   }
 `
 
-const Nav = ()=>{
+const Nav = () => {
     return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name='tag'/>
-                    <Link to="/tags">标签</Link>
+                    <Link to="/tags">
+                        <Icon name='tag'/>
+                        标签
+                    </Link>
                 </li>
                 <li>
-                    <Icon name='money'/>
-                    <Link to="/money">记账</Link>
+                    <Link to="/money">
+                        <Icon name='money'/>
+                        记账
+                    </Link>
                 </li>
                 <li>
-                    <Icon name='chart'/>
-                    <Link to="/statistics">统计</Link>
+                    <Link to="/statistics">
+                        <Icon name='chart'/>
+                        统计
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
