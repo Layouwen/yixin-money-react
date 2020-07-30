b8da9a28040fb5c0e18361f0116413fb31f1caf6
layouwen <layouwen@gmail.com>
2020-07-30 10:57:40 +0800

    完成Money页面收入支出类型模块样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -44,7 +44,27 @@ const NotesSection = styled.section`
   }
 `
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li {
+      position: relative;
+      padding: 16px 0;
+      width: 50%;
+      text-align: center;
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        display: block;
+        height: 3px;
+        width: 100%;
+        background: #333;
+      }
+    }
+  }
 `
 const NumberPadSection = styled.section`
 
@@ -70,7 +90,7 @@ function Money() {
             </NotesSection>
             <CategorySection>
                 <ul>
-                    <li>支出</li>
+                    <li className="selected">支出</li>
                     <li>收入</li>
                 </ul>
             </CategorySection>
