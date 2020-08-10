9fd1c7f22ae15302e9205b60f10758d0bce34454
layouwen <layouwen@gmail.com>
2020-08-11 02:09:51 +0800

    分装 generateOuput 函数，进行设置output

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,52 +1,26 @@
 import React, {useState} from 'react';
-import {Wrapper} from './NumberPadSection/Wrapper'
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
-  const setOutput = (output:string)=>{
-    if(output.length > 16){
-      output = output.slice(0, 16)
-    }else if(output.length === 0){
-       output = '0'
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
     }
-    _setOutput(output)
-  }
+    _setOutput(output);
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if(output.indexOf('.')>0){return}
-        setOutput(output + '.')
-        break;
-      case '删除':
-        if(output.length===1){
-          setOutput('')
-        }else{
-          setOutput(output.slice(0, -1))
-        }
-        break;
-      case '清空':
-        setOutput('')
-        break;
-      case '确认':
-        break;
+    if (text === 'OK') {
+      // TODO
+      return;
+    }
+    if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output));
     }
   };
   return (
