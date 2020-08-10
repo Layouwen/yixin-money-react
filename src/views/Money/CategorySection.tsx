1bcc9f916425d9e62101ea30272aedf799904e30
layouwen <layouwen@gmail.com>
2020-08-11 01:16:04 +0800

    补充key

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -35,7 +35,7 @@ const CategorySection: React.FC = () => {
     <Wrapper>
       <ul>
         {categoryList.map(c =>
-          <li className={category === c ? 'selected' : ''}
+          <li key={c} className={category === c ? 'selected' : ''}
               onClick={() => setCategory(c)}>
             {categoryMap[c]}
           </li>
