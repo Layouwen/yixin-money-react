e3f74937ed7e17e36137648db9147d5ca65c9640
layouwen <layouwen@gmail.com>
2020-08-10 23:36:19 +0800

    完成按钮的创建的新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import React from 'react'
+import React, {useState} from 'react';
 
-const _TagsSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
@@ -18,6 +18,9 @@ const _TagsSection = styled.section`
         font-size: 14px;
         border-radius: 18px;
         background: #d9d9d9;
+        &.selected {
+          background: #f60;
+        }
       }
   }
   > button {
@@ -28,24 +31,34 @@ const _TagsSection = styled.section`
     border-bottom: 1px solid #333;
     background: none;
   }
-`
+`;
 
-const TagsSection: React.FunctionComponent = () => {
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const onAddTag = () => {
+    const tagName = window.prompt('新标签的名称为');
+    if (tagName !== null) {setTags([...tags, tagName]);}
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
   return (
-    <div>
-      <_TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新建标签</button>
-      </_TagsSection>
-    </div>
-  )
-}
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={() => onToggleTag(tag)} className={getClass(tag)}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新建标签</button>
+    </Wrapper>
+  );
+};
 
-
-
-export {TagsSection}
+export {TagsSection};
