e102e69cd935e9307e60b041747908b730b1572a
layouwen <layouwen@gmail.com>
2020-08-14 01:11:51 +0800

    完成 tag list 的样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,14 +1,63 @@
-import Layout from "../components/Layout";
-import React from "react";
+import Layout from '../components/Layout';
+import React from 'react';
 import {useTags} from 'useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TagList = styled.ol`
+  font-size: 16px;
+  > li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 20px;
+    border-bottom: 1px solid #d5d5d9;
+  }
+`;
+
+const Button = styled.button`
+  padding: 8px 13px;
+  font-size: 18px;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  background: #f60;
+`;
+
+const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Space = styled.div`
+  height: 16px;
+`
 
 function Tags() {
-    const {tags, setTags} = useTags()
-    return (
-        <Layout>
-            <h2>标签</h2>
-        </Layout>
-    );
+  const {tags, setTags} = useTags();
+  return (
+    <Layout>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right"/>
+          </li>
+        )}
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+        <Space/>
+      </Center>
+    </Layout>
+  );
 }
 
 export default Tags;
