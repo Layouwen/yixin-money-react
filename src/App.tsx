7c22ae9c646319be5e2a06487c4f06bb9bed6fdd
layouwen <layouwen@gmail.com>
2020-08-14 02:10:31 +0800

    使用Link路由到对应页面，学会 exact 精确匹配的用法

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,46 @@
 import React from 'react';
 import {
-    HashRouter as Router,
-    Switch,
-    Route,
-    Redirect
-} from "react-router-dom";
-import Tags from "./views/Tags";
-import Money from "./views/Money";
-import Statistics from "./views/Statistics";
-import NoMatch from "./views/NoMatch";
-import styled from "styled-components";
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
+import {Tag} from './views/Tag';
 
 const AppWrapper = styled.div`
   color: #333;
-`
+`;
 
 function App() {
-    return (
-        <AppWrapper>
-            <Router>
-                <Switch>
-                    <Route path="/tags">
-                        <Tags/>
-                    </Route>
-                    <Route path="/money">
-                        <Money/>
-                    </Route>
-                    <Route path="/statistics">
-                        <Statistics/>
-                    </Route>
-                    <Redirect exact from="/" to="/money"/>
-                    <Route path="*">
-                        <NoMatch/>
-                    </Route>
-                </Switch>
-            </Router>
-        </AppWrapper>
-    );
+  return (
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route exact path="/tags">
+            <Tags/>
+          </Route>
+          <Route exact path="/tags/:tag">
+            <Tag/>
+          </Route>
+          <Route exact path="/money">
+            <Money/>
+          </Route>
+          <Route exact path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
+  );
 }
 
 export default App;
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 import {useTags} from 'useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
   > li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     line-height: 20px;
     padding: 12px 16px 12px 0;
     margin-left: 20px;
     border-bottom: 1px solid #d5d5d9;
+    > a {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
 `;
 
@@ -35,7 +38,7 @@ const Center = styled.div`
 
 const Space = styled.div`
   height: 16px;
-`
+`;
 
 function Tags() {
   const {tags, setTags} = useTags();
@@ -44,8 +47,10 @@ function Tags() {
       <TagList>
         {tags.map(tag =>
           <li key={tag}>
-            <span className="oneLine">{tag}</span>
-            <Icon name="right"/>
+            <Link to={'/tags/' + tag}>
+              <span className="oneLine">{tag}</span>
+              <Icon name="right"/>
+            </Link>
           </li>
         )}
       </TagList>
