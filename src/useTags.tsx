acab83e4ed83a87bf41b40456948b718f07a10d6
layouwen <layouwen@gmail.com>
2020-08-14 00:21:53 +0800

    抽离 useTags 函数

---
+++ src/useTags.tsx
@@ -0,0 +1,6 @@
+import {useState} from 'react';
+const useTags = ()=>{
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  return {tags,setTags}
+}
+export {useTags}
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,7 +1,9 @@
 import Layout from "../components/Layout";
 import React from "react";
+import {useTags} from 'useTags';
 
 function Tags() {
+    const {tags, setTags} = useTags()
     return (
         <Layout>
             <h2>标签</h2>
