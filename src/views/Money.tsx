da2ec073f21c09f58ad815886a31890a8a7f0352
layouwen <layouwen@gmail.com>
2020-08-22 01:32:19 +0800

    解决无法输入 . 的bug

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import {CategorySection} from './Money/CategorySection';
 import {NotesSection} from './Money/NotesSection';
@@ -38,7 +38,11 @@ function Money() {
       setSelected(defaultFormData);
     }
   };
-
+  useEffect(() => {
+    setTimeout((
+      setSelected({...selected, amount: 1000})
+    ), 3000);
+  }, []);
   return (
     <MyLayout>
       <TagsSection value={selected.tagIds}
@@ -48,7 +52,7 @@ function Money() {
       <CategorySection value={selected.category}
                        onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={amount => onChange({amount})}
+                        onChange={value => onChange({amount: value})}
                         onOk={submit}
       />
     </MyLayout>
--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Wrapper} from './NumberPadSection/Wrapper';
 import {generateOutput} from './NumberPadSection/generateOutput';
 
 type Props = {
   value: number,
   onChange: (value: number) => void,
-  onOk?: ()=>void
+  onOk?: () => void
 }
 
 const NumberPadSection: React.FC<Props> = (props) => {
-  const output = props.value.toString()
+  // const output = props.value.toString()
+  const [output, _setOutput] = useState(props.value.toString());
   const setOutput = (output: string) => {
-    let value
+    let newOutput: string;
     if (output.length > 16) {
-      value = parseFloat(output.slice(0, 16));
+      newOutput = output.slice(0, 16);
     } else if (output.length === 0) {
-      value = 0;
-    }else {
-      value= parseFloat(output)
+      newOutput = '0';
+    } else {
+      newOutput = output;
     }
-    props.onChange(value);
+    _setOutput(newOutput);
+    props.onChange(parseFloat(newOutput));
   };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) {return;}
     if (text === 'OK') {
-      if(props.onOk){props.onOk()}
+      if (props.onOk) {props.onOk();}
       return;
     }
     if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
