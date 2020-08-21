3e0e374a6bfd3c13004111486262ab3039999e50
layouwen <layouwen@gmail.com>
2020-08-21 13:51:43 +0800

    封装 findTag 到 useTags 里

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom'
+
+type Params = {
+  id: string
+}
 
 const Tag: React.FC = () =>{
+  const {findTag} = useTags()
+  let {id} = useParams<Params>()
+
+  const tag = findTag(parseInt(id))
   return (
-    <div>h1</div>
+    <div>{tag.name}</div>
   );
 }
 
